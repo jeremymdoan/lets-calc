b09ad7a03d17d93447bd3f53b5d83891084a36ea
Jeremy Doan <jeremy.doan@ihsmarkit.com>
2023-02-22 08:47:31 -0700

    some adjustments

--- theDatabase/anagrams.py
+++ theDatabase/anagrams.py
@@ -3,17 +3,22 @@ from itertools import permutations
 def next_alpha(s):
     return chr((ord(s.upper())+1 - 65) % 26 + 65)
 
-def change_last_letter(word):
-    return word[0:-1] + next_alpha(word[-1]).lower()
+def change_letter(word, n):
+    out = word[0:n] + next_alpha(word[n]).lower()
+    return out + word[n+1:] if n < len(word) else out
 
 def anagrams(word = 'database'):
+    word = word.strip()
     r = []
     R = []
+    n = 0
     for p in permutations(word):
-        if len(r) > 12:
+        if len(r) == len(word):
             R.append(r)
             r = []
+            n = 0
         w = ''.join(p)
-        r.append((w, change_last_letter(w)))
+        r.append((w, change_letter(w, n)))
+        n += 1
     R.append(r)
     return { 'rows': R, 'word': word}
